a7cf104a28e6eb7480235415e8a81cdee7feabe4
Cystemz <72627466+Cystemzz@users.noreply.github.com>
2023-12-21 23:13:44 -0500

    Create database model

---
+++ tracker/structs.go
@@ -0,0 +1,30 @@
+package tracker
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Workspace struct {
+	Name       string
+	Location   string
+	LastLoaded time.Time
+	Version    int
+}
+
+type Job struct {
+	gorm.Model
+	Name        string
+	Description string
+	Rate        float32
+	TimeCards   []TimeCard
+}
+
+type TimeCard struct {
+	gorm.Model
+	Started      time.Time
+	Ended        time.Time
+	LastExported time.Time
+	Notes        string
+	JobID        uint
+}
